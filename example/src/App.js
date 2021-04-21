4a590cfec7b9f83f936d6aa12e5a7479eedbc6f6
eddsonrt <eddsonrt@hotmail.com>
2021-04-21 17:19:04 -0500

    chore: removing example from eslintignore

--- example/src/App.js
+++ example/src/App.js
@@ -1,46 +1,64 @@
 import React from 'react'
-import { StoreCard, LockerCard, BattlepassCard } from 'fortnite-components'
+import {
+  StoreCard,
+  LockerCard,
+  BattlepassCard,
+  AchievementCard
+} from 'fortnite-components'
 
 const App = () => {
   return (
     <>
       <h1>StoreCard</h1>
       <StoreCard
-        label="Safari"
-        type="Outfit"
-        rarity="rare"
-        size="medium"
-        image="https://media.fortniteapi.io/images/displayAssets/v2/DAv2_CID_802_f_HeartBreaker/MI_CID_802_f_HeartBreaker.png"
+        label='Safari'
+        type='Outfit'
+        rarity='rare'
+        size='medium'
+        image='https://media.fortniteapi.io/images/displayAssets/v2/DAv2_CID_802_f_HeartBreaker/MI_CID_802_f_HeartBreaker.png'
         shopVersion={false}
-        priceType="vBucks"
-        tag="none"
-        banner="none"
-        bannerText="New!"
+        priceType='vBucks'
+        tag='none'
+        banner='none'
+        bannerText='New!'
         price={1200}
         fullPrice={1200}
-        passText="Chapter 1, Season 7"
-        passType="paid"
-        money="20.00"
-        moreText="More Info"
+        passText='Chapter 1, Season 7'
+        passType='paid'
+        money='20.00'
+        moreText='More Info'
         onClick={() => {}}
       />
       <h1>LockerCard</h1>
       <LockerCard
-        rarity="rare"
-        size="small"
-        image="https://media.fortniteapi.io/images/82805d371b0c5d8e61b410efc8c81bbd/transparent.png"
-        tag="none"
-        banner="none"
-        bannerText="New!"
+        rarity='rare'
+        size='small'
+        image='https://media.fortniteapi.io/images/82805d371b0c5d8e61b410efc8c81bbd/transparent.png'
+        tag='none'
+        banner='none'
+        bannerText='New!'
         onClick={() => {}}
       />
       <h1>BattlepassCard</h1>
       <BattlepassCard
-        rarity="rare"
+        rarity='rare'
         tier={12}
         free={false}
-        freeText="Free!"
-        image="https://media.fortniteapi.io/images/82805d371b0c5d8e61b410efc8c81bbd/transparent.png"
+        freeText='Free!'
+        image='https://media.fortniteapi.io/images/82805d371b0c5d8e61b410efc8c81bbd/transparent.png'
+        onClick={() => {}}
+      />
+      <h1>AchievementCard</h1>
+      <AchievementCard
+        name='Pocket change'
+        image='https://media.fortniteapi.io/images/feats/s16_T_Ui_Explore_Comical.png'
+        description='Collected all the coins from a Purple XP Coin during Season 6.'
+        time='10:52 PM'
+        topColor='#7c3aed'
+        bottomColor='#4F8EFF'
+        imageColor='#7c3aed'
+        topBackground='#326DD7'
+        bottomBackground='#3A39D0'
         onClick={() => {}}
       />
     </>
