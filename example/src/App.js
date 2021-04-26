b2dbf80b6dfa5ca410fb5b8b63f6822cba4931a4
eddsonrt <eddsonrt@hotmail.com>
2021-04-26 12:38:38 -0500

    chore: fixed example

--- example/src/App.js
+++ example/src/App.js
@@ -5,24 +5,41 @@ import {
   BattlepassCard,
   AchievementCard,
   FishCard,
-  EventCard
-} from 'fortnite-components/'
+  EventCard,
+  CharacterCard
+} from 'fortnite-components'
 
 const App = () => {
   return (
-    <div style={{ background: 'black', color: '#fff' }}>
-      <h1>StoreCard</h1>
-      <StoreCard />
-      <h1>LockerCard</h1>
-      <LockerCard />
-      <h1>BattlepassCard</h1>
-      <BattlepassCard />
-      <h1>AchievementCard</h1>
-      <AchievementCard />
-      <h1>FishCard</h1>
-      <FishCard />
-      <h1>EventCard</h1>
-      <EventCard />
+    <div className='all'>
+      <div className='item'>
+        <h1>StoreCard</h1>
+        <StoreCard />
+      </div>
+      <div className='item'>
+        <h1>EventCard</h1>
+        <EventCard />
+      </div>
+      <div className='item'>
+        <h1>AchievementCard</h1>
+        <AchievementCard />
+      </div>
+      <div className='item'>
+        <h1>LockerCard</h1>
+        <LockerCard />
+      </div>
+      <div className='item'>
+        <h1>CharacterCard</h1>
+        <CharacterCard />
+      </div>
+      <div className='item'>
+        <h1>BattlepassCard</h1>
+        <BattlepassCard />
+      </div>
+      <div className='item'>
+        <h1>FishCard</h1>
+        <FishCard />
+      </div>
     </div>
   )
 }
--- example/src/index.js
+++ example/src/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import GlobalStyles from './styles/GlobalStyles'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <GlobalStyles />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
