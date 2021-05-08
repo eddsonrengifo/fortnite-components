41f6821899f677909dbf2b763808aa3379872a13
eddsonrt <eddsonrt@hotmail.com>
2021-05-07 19:14:43 -0500

    chore: updated config eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,74 +1,68 @@
 module.exports = {
-  parser: 'eslint-multiple-parsers',
-  plugins: ['@typescript-eslint', '@emotion'],
-  env: {
-    node: true,
-    jest: true,
-    es6: true,
-    browser: true
-  },
-  extends: [
-    'standard',
-    'standard-react',
-    // 'plugin:prettier/recommended',
-    // 'prettier/standard'
-    // 'prettier/react',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'eslint:recommended',
-    'plugin:react/recommended'
-    // 'plugin:@typescript-eslint/recommended'
-  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    parsers: [
-      {
-        test: '.*\\.js$',
-        path: 'babel-eslint',
-        options: {
-          sourceType: 'module'
-        }
-      },
-      {
-        test: '.*\\.ts$',
-        path: '@typescript-eslint/parser'
-      }
-    ],
     ecmaVersion: 2020,
+    sourceType: 'module',
     ecmaFeatures: {
-      legacyDecorators: true,
       jsx: true
     }
   },
+  plugins: ['prettier', '@typescript-eslint'],
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'standard',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
   settings: {
     react: {
       version: 'detect'
     }
   },
   rules: {
-    'space-before-function-paren': 0,
-    'react/prop-types': 0,
-    'react/jsx-handler-names': 0,
-    'react/jsx-fragments': 0,
-    'react/no-unused-prop-types': 0,
-    'import/export': 0,
-    'react/display-name': 0,
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/explicit-member-accessibility': 0,
-    '@typescript-eslint/indent': 0,
-    '@typescript-eslint/member-delimiter-style': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/no-use-before-define': 0,
-    '@typescript-eslint/no-unused-vars': [
-      2,
+    '@typescript-eslint/explicit-module-boundary-types': [
+      'off',
       {
-        argsIgnorePattern: '^_'
+        allowedNames: ['ignoredFunctionName', 'ignoredMethodName']
       }
     ],
-    'no-console': [
-      2,
-      {
-        allow: ['warn', 'error']
-      }
-    ]
+    '@typescript-eslint/ban-ts-ignore': ['off'],
+    '@typescript-eslint/camelcase': ['off'],
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/interface-name-prefix': ['off'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-unused-expressions': ['off'],
+    '@typescript-eslint/no-var-requires': ['off'],
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'no-use-before-define': 'off',
+    'comma-dangle': ['error', 'never'],
+    'no-async-promise-executor': ['off'],
+    'no-empty-pattern': ['off'],
+    'no-undef': ['error'],
+    'no-var': ['error'],
+    'object-curly-spacing': ['error', 'always'],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    semi: ['error', 'never'],
+    'spaced-comment': ['off'],
+    'no-prototype-builtins': ['off'],
+    'sort-keys': ['off'],
+    'space-before-function-paren': ['off'],
+    indent: ['off']
+  },
+  globals: {
+    React: true,
+    google: true,
+    mount: true,
+    mountWithRouter: true,
+    shallow: true,
+    shallowWithRouter: true,
+    context: true,
+    expect: true,
+    jsdom: true,
+    JSX: true
   }
 }
