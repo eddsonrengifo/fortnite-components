23a4fbf1c2e0b2070c2edd48de52ec4c9fd41f16
eddsonrt <eddsonrt@hotmail.com>
2021-05-26 22:09:30 -0500

    chore: font now for cell phones

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -3,7 +3,20 @@ import { Global, css } from '@emotion/react'
 import { GlobalStyles as BaseStyles } from 'twin.macro'
 
 const customStyles = css`
-  @import url(//db.onlinewebfonts.com/c/cdc8c3de1e6bd5a9a67c6a001f9fedd0?family=Burbank+Big+Condensed);
+  @font-face {
+    font-family: 'Burbank Big Condensed';
+    src: url('//db.onlinewebfonts.com/t/cdc8c3de1e6bd5a9a67c6a001f9fedd0.eot');
+    src: url('//db.onlinewebfonts.com/t/cdc8c3de1e6bd5a9a67c6a001f9fedd0.eot?#iefix')
+        format('embedded-opentype'),
+      url('//db.onlinewebfonts.com/t/cdc8c3de1e6bd5a9a67c6a001f9fedd0.woff2')
+        format('woff2'),
+      url('//db.onlinewebfonts.com/t/cdc8c3de1e6bd5a9a67c6a001f9fedd0.woff')
+        format('woff'),
+      url('//db.onlinewebfonts.com/t/cdc8c3de1e6bd5a9a67c6a001f9fedd0.ttf')
+        format('truetype'),
+      url('//db.onlinewebfonts.com/t/cdc8c3de1e6bd5a9a67c6a001f9fedd0.svg#Burbank Big Condensed')
+        format('svg');
+  }
 `
 
 const GlobalStyles = () => (
