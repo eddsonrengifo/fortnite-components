24f1b21c6920e29f3bbe771f058fc5d00d1ce75a
eddsonrt <eddsonrt@hotmail.com>
2021-09-03 17:50:32 -0500

    chore: storecard added in components tag

--- .storybook/main.js
+++ .storybook/main.js
@@ -21,6 +21,7 @@ function getPackageDir(filepath) {
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+
   webpackFinal: async (config) => {
     return merge(config, {
       resolve: {
